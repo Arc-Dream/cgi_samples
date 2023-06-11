31eaa6bd33c9e2b7c723d1ed20949c69e241ce4b
Dreamspark <73857862+Arc-Dream@users.noreply.github.com>
2023-06-11 20:24:58 +0300

    Add files via upload

--- perl_api/index.pl
+++ perl_api/index.pl
@@ -1,60 +1,116 @@
 #!C:\xampp\perl\bin\perl.exe
 
-
-#GET REQUEST FETCH DATA
-$get_url = $ENV{'QUERY_STRING'};
-@pairs = split(/&/, $get_url);
+use DBI;
 
 
-#POST REQUEST FETCH DATA
-read(STDIN, $buffer, $ENV{'CONTENT_LENGTH'});
-@pairs_post = split(/&/, $buffer);
+### DATABASE SECTOR
+
+    ## SET CONNECTION
+
+        my $dsn = "DBI:mysql:data_try";
+        my $db_username = "root";
+        my $db_password = "123";
+
+        my %attr = ( PrintError=>0,  RaiseError=>1);
+
+        my $dbh = DBI -> connect($dsn, $db_username, $db_password, \%attr);
+
+    ## GET ALL
+
+        my $sql = "SELECT * FROM data_try1";
+
+        my $stmt = $dbh->prepare($sql);
+
+        $stmt->execute();
 
 
 
-print "Content-type: text/html; charset=iso-8859-1\n\n";
+
+### AUTHENTICATION SECTOR
+
+    ## SET VARIABLES
+
+        my $auth = 0;
+
+        @current_user = ('','');
 
 
-$head =     "
-                <head>
-                    <title>PERL</title>
-                </head>
-            ";
+    ## FETCH POST REQUEST DATA
 
-$style =    "
-                <style>
-                    body 
+        read(STDIN, $buffer, $ENV{'CONTENT_LENGTH'});
+        @pairs_post = split(/&/, $buffer);
+
+        @pairs_post_email = split(/=/, @pairs_post[0]);
+        @pairs_post_password = split(/=/, @pairs_post[1]);
+
+        
+
+        # print "{@pairs_post}";
+
+
+    ## PROCESS FETCHED DATA
+
+        if (@pairs_post_email[0] eq "email")
+        {
+            @current_user[0] = @pairs_post_email[1];
+        }
+
+        if (@pairs_post_password[0] eq "password")
+        {
+            @current_user[1] = @pairs_post_password[1];
+        }
+
+
+    ## EXECUTE AUTHETICATION
+
+        if ((@current_user[0] eq "aa") && (@current_user[1] eq "123"))
+        {
+            $auth = 1;
+        }
+
+
+
+### DISPLAY SECTOR
+
+    ## COMPARE AUTH VARIABLE
+
+        if ($auth == 1)
+        {
+            print "Content-type: application/json; charset=iso-8859-1\n\n";
+
+            @api_result = ();
+
+            if ($ENV{'REQUEST_METHOD'} == "POST")
+            {
+                  
+                while (my @array_result = $stmt->fetchrow_array())
+                {
+                    $data = qq-
                     {
-                        background-color :#333;
-                        color: #AAA;
+                        "id" : "$array_result[0]",
+                        "name" : "$array_result[1]",
+                        "email" : "$array_result[2]",
+                        "message" : "$array_result[3]"
                     }
-                </style>
-            ";
+                    -;    
 
-$body =     "
-                <body>
-                    <h1>Hello World</h1>
-                </body>
-            ";
+                    push (@api_result, $data);
+                }
 
+                print @api_result;
 
-print $head1;
-print $style;
-print $body;
+                return @api_result;
+            }          
+        }
 
-foreach $pair (@pairs)
-{
-    ($key, $value) = split(/=/, $pair);
+        else 
+        {
+            print "Content-type: application/json; charset=iso-8859-1\n\n";
 
-    print "<p>$key = $value</p>";
-}
+            print '{"error":"no authentication"}';
 
-foreach $pair (@pairs_post)
-{
-    ($key, $value) = split(/=/, $pair);
+        }
 
-    print "<p>$key = $value</p>";
-}
 
 
 
